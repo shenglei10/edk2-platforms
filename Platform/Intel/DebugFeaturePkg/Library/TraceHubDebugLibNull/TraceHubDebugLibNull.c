1830e20e9e5c1b62257cb1ef1c43ef18f1b4f437
Shenglei Zhang <shenglei.zhang@intel.com>
2019-06-04 09:08:46 +0800

    Platform/DebugFeaturePkg: Add a library instance TraceHubDebugLibNull
    Signed-off-by: Shenglei Zhang <shenglei.zhang@intel.com>

---
+++ Platform/Intel/DebugFeaturePkg/Library/TraceHubDebugLibNull/TraceHubDebugLibNull.c
@@ -0,0 +1,193 @@
+/** @file
+  Null instance of TraceHub supporting function
+
+  Copyright (c) 2019, Intel Corporation. All rights reserved.<BR>
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#include <Base.h>
+#include <Library/DebugLib.h>
+#include <Library/TraceHubDebugLib.h>
+
+/**
+  Write data from buffer and Severity to TraceHub.
+
+  Writes NumberOfBytes data bytes from Buffer and Severity to TraceHub.
+  If Buffer is NULL and NumberOfBytes is larger than zero, then ASSERT().
+  If NumberOfBytes is zero, no buffer is written.
+
+  @param  SeverityType     Severity type of the written data buffer.
+  @param  Buffer           Pointer to the data buffer to be written.
+  @param  NumberOfBytes    Number of bytes to written to TraceHub device.
+
+  @retval RETURN_SUCCESS   The number of bytes written to TraceHub.
+  @retval Other            Fail to write the buffer to TraceHub.
+
+**/
+RETURN_STATUS
+EFIAPI
+TraceHubDebugWrite (
+  IN TRACE_HUB_SEVERITY_TYPE    SeverityType,
+  IN UINT8                      *Buffer,
+  IN UINTN                      NumberOfBytes
+  )
+{
+  ASSERT (Buffer != NULL || NumberOfBytes == 0);
+
+  return RETURN_UNSUPPORTED;
+}
+
+/**
+  Write catalog64 message SVEN event with 0 parameters to TraceHub
+
+  @param  SeverityType   Severity type of the catid message event.
+  @param  Id             Catalog ID
+
+**/
+VOID
+EFIAPI
+TraceHubWriteCataLog64_0 (
+  IN TRACE_HUB_SEVERITY_TYPE    SeverityType,
+  IN UINT64                     Id
+  )
+{
+}
+
+/**
+  Write catalog64 message SVEN event with 1 parameters to TraceHub
+
+  @param  SeverityType   Severity type of the catid message event.
+  @param  Id             Catalog ID
+  @param  Param1         Parameter 1 passed to catid message event.
+
+**/
+VOID
+EFIAPI
+TraceHubWriteCataLog64_1 (
+  IN TRACE_HUB_SEVERITY_TYPE    SeverityType,
+  IN UINT64                     Id,
+  IN UINT32                     Param1
+  )
+{
+}
+
+/**
+  Write catalog64 message SVEN event with 6 parameters to TraceHub
+
+  @param  SeverityType   Severity type of the catid message event.
+  @param  Id             Catalog ID
+  @param  Param1         Parameter 1 passed to catid message event.
+  @param  Param2         Parameter 2 passed to catid message event.
+
+**/
+VOID
+EFIAPI
+TraceHubWriteCataLog64_2 (
+  IN TRACE_HUB_SEVERITY_TYPE    SeverityType,
+  IN UINT64                     Id,
+  IN UINT32                     Param1,
+  IN UINT32                     Param2
+  )
+{
+}
+
+/**
+  Write catalog64 message SVEN event with 3 parameters to TraceHub
+
+  @param  SeverityType   Severity type of the catid message event.
+  @param  Id             Catalog ID
+  @param  Param1         Parameter 1 passed to catid message event.
+  @param  Param2         Parameter 2 passed to catid message event.
+  @param  Param3         Parameter 3 passed to catid message event.
+
+**/
+VOID
+EFIAPI
+TraceHubWriteCataLog64_3 (
+  IN TRACE_HUB_SEVERITY_TYPE    SeverityType,
+  IN UINT64                     Id,
+  IN UINT32                     Param1,
+  IN UINT32                     Param2,
+  IN UINT32                     Param3
+  )
+{
+}
+
+/**
+  Write catalog64 message SVEN event with 4 parameters to TraceHub
+
+  @param  SeverityType   Severity type of the catid message event.
+  @param  Id             Catalog ID
+  @param  Param1         Parameter 1 passed to catid message event.
+  @param  Param2         Parameter 2 passed to catid message event.
+  @param  Param3         Parameter 3 passed to catid message event.
+  @param  Param4         Parameter 4 passed to catid message event.
+
+**/
+VOID
+EFIAPI
+TraceHubWriteCataLog64_4 (
+  IN TRACE_HUB_SEVERITY_TYPE    SeverityType,
+  IN UINT64                     Id,
+  IN UINT32                     Param1,
+  IN UINT32                     Param2,
+  IN UINT32                     Param3,
+  IN UINT32                     Param4
+  )
+{
+}
+
+/**
+  Write catalog64 message SVEN event with 5 parameters to TraceHub
+
+  @param  SeverityType   Severity type of the catid message event.
+  @param  Id             Catalog ID
+  @param  Param1         Parameter 1 passed to catid message event.
+  @param  Param2         Parameter 2 passed to catid message event.
+  @param  Param3         Parameter 3 passed to catid message event.
+  @param  Param4         Parameter 4 passed to catid message event.
+  @param  Param5         Parameter 5 passed to catid message event.
+
+**/
+VOID
+EFIAPI
+TraceHubWriteCataLog64_5 (
+  IN TRACE_HUB_SEVERITY_TYPE    SeverityType,
+  IN UINT64                     Id,
+  IN UINT32                     Param1,
+  IN UINT32                     Param2,
+  IN UINT32                     Param3,
+  IN UINT32                     Param4,
+  IN UINT32                     Param5
+  )
+{
+}
+
+/**
+  Write catalog64 message SVEN event with 6 parameters to TraceHub
+
+  @param  SeverityType   Severity type of the catid message event.
+  @param  Id             Catalog ID
+  @param  Param1         Parameter 1 passed to catid message event.
+  @param  Param2         Parameter 2 passed to catid message event.
+  @param  Param3         Parameter 3 passed to catid message event.
+  @param  Param4         Parameter 4 passed to catid message event.
+  @param  Param5         Parameter 5 passed to catid message event.
+  @param  Param6         Parameter 6 passed to catid message event.
+
+**/
+VOID
+EFIAPI
+TraceHubWriteCataLog64_6 (
+  IN TRACE_HUB_SEVERITY_TYPE    SeverityType,
+  IN UINT64                     Id,
+  IN UINT32                     Param1,
+  IN UINT32                     Param2,
+  IN UINT32                     Param3,
+  IN UINT32                     Param4,
+  IN UINT32                     Param5,
+  IN UINT32                     Param6
+  )
+{
+}
